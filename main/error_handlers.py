1cea141754b92d51832d0d01a51f3e6a3cef56af
Zac Lo <zlo120@live.com>
2021-10-16 15:30:12 +1000

    added error pages

--- main/error_handlers.py
+++ main/error_handlers.py
@@ -3,8 +3,8 @@ from . import app
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("error_handlers/page_not_found.html")
+    return render_template("error_handlers/Error.html")
     
 @app.errorhandler(401)
 def not_authenticated(e):
-    return render_template("error_handlers/not_authenticated.html")
+    return render_template("error_handlers/Error.html")
